4b97598c885d1c588f3ba42f3eeed37ffc88f0f6
Achraf Gharbi <achraf.gharbi@student.ecp.fr>
2016-03-30 09:26:06 +0200

    Plus de Fouad-Sams

--- Group2_Project_IS1220_part1_GharbiLaaboudi/src/interfaces/Offer.java
+++ Group2_Project_IS1220_part1_GharbiLaaboudi/src/interfaces/Offer.java
@@ -3,8 +3,13 @@ package interfaces;
 import classes.Order;
 
 /**
+<<<<<<< Updated upstream
  * This interface enables us to use the pattern strategy. The method discountedPrice returns for a given order the
  * price that have been removed from the initial price.
+=======
+ * @author Younes Laaboudi
+ * @author Achraf Gharbi
+>>>>>>> Stashed changes
  *
  */
 public interface Offer {
